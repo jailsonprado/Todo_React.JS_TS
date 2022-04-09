bd29f4dbf46b637b56f42c652ec3357f67953b37
Jailson Prado <jailsondpd@gmail.com>
2022-04-09 18:45:37 -0300

    create header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 
+// components
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Todo TS</h1>
+    <div>
+      <Header />
+      <h1>Conteudo</h1>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer>
+        <p>
+          <span>React + TS Todo</span> @ 2022
+        </p>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
