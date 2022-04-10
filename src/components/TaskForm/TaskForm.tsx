2e7e73d0aa0e0f922e481f02414dcce70f390e5f
Jailson Prado <jailsondpd@gmail.com>
2022-04-09 22:01:19 -0300

    style for form

--- src/components/TaskForm/TaskForm.tsx
+++ src/components/TaskForm/TaskForm.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 
+// styles
+import styles from "./taskForm.module.css";
+
 interface Props {
   btnText: string;
 }
 
 const TaskForm = ({ btnText }: Props) => {
   return (
-    <form>
-      <div>
+    <form className={styles.form}>
+      <div className={styles.inputGroup}>
         <label htmlFor="title">Titulo:</label>
         <input type="text" name="title" placeholder="Titulo da tarefa" />
       </div>
-      <div>
+      <div className={styles.inputGroup}>
         <label htmlFor="difficulty">Dificuldade:</label>
         <input
           type="text"
